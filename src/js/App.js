05dda149af2198f2c7bb407200dd17d1c049f10d
Elise Cicognani <e.cicognani@cantierecreativo.net>
2015-09-07 16:24:40 +0200

    Setting extraverted functions' system

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,53 @@
 var React = require('react');
+var Extraverted = require("./Extraverted");
+var Introverted = require("./Introverted");
+var Domrep = require("./Domrep");
+var Result = require("./Result");
 var PropTypes = React.PropTypes;
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      view: "extraverted",
+      perceptiveExtr: null,
+      judgingExtr: null
+    };
+  },
+
+  handleView: function() {
+    switch (this.state.view) {
+      case "extraverted":
+        return <Extraverted
+          judgingEx={ this.handleJudgingChange }
+          perceptiveEx={ this.handlePerceptiveChange }
+          />;
+      case "introverted":
+        return <Introverted />;
+      case "domrep":
+        return <Domrep />;
+      case "result":
+        return <Result />;
+    }
+  },
+
+  handleJudgingChange: function(whatiwant) {
+    this.setState({
+      judgingExtr: whatiwant
+    });
+  },
+
+  handlePerceptiveChange: function(whatiwant) {
+    this.setState({
+      perceptiveExtr: whatiwant
+    });
+    debugger;
+  },
 
   render: function() {
     return (
-      <div />
+      <div>
+        { this.handleView() }
+      </div>
     );
   }
 
---
+++ src/js/Extraverted.js
@@ -0,0 +1,66 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Extraverted = React.createClass({
+  getInitialState: function() {
+    return {
+      perceptiveExtr: null,
+      judgingExtr: null
+    };
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h1>Vediamo se funzionano le cose</h1>
+        <p className="question">Qui ci va la domanda per la funzione estroversa percettiva</p>
+        <form>
+          <input
+            type="radio"
+            name="perceptive-extr"
+            onChange={ this.handlePerceptiveChange }
+            value="Ne"
+            >
+            Intuizione estroversa
+          </input>
+          <br></br>
+          <input
+            type="radio"
+            name="perceptive-extr"
+            onChange={ this.handlePerceptiveChange }
+            value="Se"
+            >Sensazione estroversa</input>
+        </form>
+        <p className="question">Qui ci va la domanda per la funzione estroversa giudicante</p>
+        <form>
+           <input
+             type="radio"
+             name="judging-extr"
+             onChange={ this.handleJudgingChange }
+             value="Te"
+             >
+             Pensiero estroverso</input>
+           <br></br>
+           <input
+             type="radio"
+             name="judging-extr"
+             onChange={ this.handleJudgingChange }
+             value="Fe"
+             >
+             Sentimento estroverso</input>
+        </form>
+      </div>
+    );
+  },
+
+  handleJudgingChange: function(e) {
+      this.props.judgingEx(e.currentTarget.value);
+  },
+
+  handlePerceptiveChange: function(e) {
+      this.props.perceptiveEx(e.currentTarget.value);
+  }
+
+  });
+
+module.exports = Extraverted;
